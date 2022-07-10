bbcb8097ed21aaeb7cc6728756e17f442e278907
lucky-committ <74575995+lucky-committ@users.noreply.github.com>
2022-07-09 19:25:40 -0500

    Update extGaHead.js

--- extGaHead.js
+++ extGaHead.js
@@ -1,1 +1,9 @@
 //externally loaded ga
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-233554531-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-233554531-1');
+</script>
